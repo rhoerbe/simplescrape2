9cfd916e4194ee3691a05cef2118c61f6911dcdc
Rainer Hörbe <r2h2@devl20.mshome.net>
2023-12-27 19:00:33 +0100

    polish

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 # if starting from 6:00, the following list would result in following times:
 # 06:00, 07:31, 08:26, 09:37, 10:24, 11:29, 12:27, 13:30, 15:15, 16:10, 17:21, 18:08, 19:13, 20:11, 21:14, 22:59, 23:54
 intervals = (91, 55, 71, 47, 65, 58, 63, 105, 55, 71, 47, 65, 58, 63, 105, 55, 71)
+initial_delay = random.randrange(0, 3)
+if debug:
+    intervals = (0, 1, 1)
+    initial_delay = 3
+
 
 def main():
     wait_until_start()
-    initial_delay = random.randrange(0, 3)
-    if debug:
-        intervals = (0, 1, 1)
-        initial_delay = 3
     logging.info(f"scraping main loop started, initial delay = {initial_delay}s")
     time.sleep(initial_delay)
     for interval in intervals:
